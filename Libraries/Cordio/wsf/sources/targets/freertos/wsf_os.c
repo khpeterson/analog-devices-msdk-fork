0e6f86eb005fceed23c8c4326259666c9a7cd3a8
Kevin Peterson <khp@alum.mit.edu>
2026-04-07 11:17:20 -0700

    #1497: run CordioM and CordioH at same priority to prevent unexpected preemption; rework wsf_timer to avoid deadlock

--- Libraries/Cordio/wsf/sources/targets/freertos/wsf_os.c
+++ Libraries/Cordio/wsf/sources/targets/freertos/wsf_os.c
@@ -60,7 +60,7 @@ WSF_CT_ASSERT(sizeof(uint32_t) == 4);
 #define WSF_DISPATCHER_MSG_STACK_SIZE									4096
 #define WSF_DISPATCHER_HND_STACK_SIZE									4096
 #define WSF_DISPATCHER_MSG_TASK_PRIORITY							configMAX_PRIORITIES - 2
-#define WSF_DISPATCHER_HND_TASK_PRIORITY							configMAX_PRIORITIES - 3
+#define WSF_DISPATCHER_HND_TASK_PRIORITY							configMAX_PRIORITIES - 2
 
 /*! \brief OS serivice function number */
 #define WSF_OS_MAX_SERVICE_FUNCTIONS                  3
